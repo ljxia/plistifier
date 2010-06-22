107acaded4b030d7bf61e5ffaf8bfde5c1048261
pcrawfor <pcrawfor@gmail.com>
2010-06-22 08:54:17 -0700

    Adding support for parsing plist data on incoming requests

--- init.rb
+++ init.rb
@@ -2,3 +2,11 @@ require 'plistifier/plist_encoding'
 require 'rails_extensions'
 
 Mime::Type.register "application/plist", :plist
+
+# add support for plist
+ActionController::Base.param_parsers[Mime::Type.lookup('application/plist')] = Proc.new do |data|
+  #turn data into a hash of attributes      
+  plist = CFPropertyList::List.new(:data => data)
+  data = CFPropertyList.native_types(plist.value)
+  data
+end
